fd86cd4890a026bb33562b449065aceecebd77b1
Ramjan <mdramjan.ict@gmail.com>
2026-04-07 09:34:22 +0600

    template added for all

---
+++ src/main.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import App from "./App";
+import "./index.css";
+import { store } from "./store/store";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+        <ToastContainer position="top-right" autoClose={3000} theme="light" />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
+);
